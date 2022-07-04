5a9d107275b5696d1da695b03191a2788f1ac95a
Starr <starrparker@live.com>
2022-07-04 00:37:24 -0700

    adding euler_5

---
+++ euler_5/src/main/java/euler_5/App.java
@@ -0,0 +1,37 @@
+package euler_5.src.main.java.euler_5;
+
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int x = 1000000000;
+        Boolean notDivisible = false;
+        List<Integer> divisibleByAll = new ArrayList<Integer>();
+        for(int i = x; i > 0; i--){
+            notDivisible = false;
+            int max = 20;
+            for(int j = 1; j <= max; j++){
+                if(i%j == 0){
+                    if(j < max){
+                        continue;
+                    }
+                    else{
+                        if(!notDivisible){
+                            divisibleByAll.add(i);
+                        }
+                    }
+                }
+                else{
+                    notDivisible = true;
+                }
+                break;
+            }
+        }
+
+        Collections.sort(divisibleByAll);
+        if(!divisibleByAll.isEmpty())
+                System.out.println(divisibleByAll.get(0));
+    }
+}
---
+++ euler_5/src/test/java/euler_5/AppTest.java
@@ -0,0 +1,20 @@
+package euler_5.src.test.java.euler_5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
