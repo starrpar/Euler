9fa18e89854a9fd10682ed9da31bcd4744dbf041
starrpar <starrparker@live.com>
2022-07-06 00:47:49 -0700

    adding euler_15

---
+++ euler_15/src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+package euler_15;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
