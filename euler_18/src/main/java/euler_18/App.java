629a1a7eb8b33c55c63170f583bc54d6cc247fa2
starrpar <starrparker@live.com>
2022-07-18 01:42:28 -0700

    euler_18 - on soln, but wrong one

--- euler_18/src/main/java/euler_18/App.java
+++ euler_18/src/main/java/euler_18/App.java
@@ -1,8 +1,14 @@
 package euler_18;
 
 import java.util.Dictionary;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
     /*
@@ -40,8 +46,90 @@ public class App {
      * requires a clever method! ;o)
      */
     public static void main(String[] args) {
-        int sum = maxPathSum();
+        int sum = maxPath();
         System.out.println(sum);
+    }
+
+    private static int maxPath() {
+        int sum = 0;
+        // Integer[] nums = { 3, 7, 4, 2, 4, 6, 8, 5, 9, 3 };
+
+        Integer[] nums = { 75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47, 65, 19, 01, 23, 75, 03, 34, 88, 02,
+                77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33,
+                47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39,
+                68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 04, 68, 89, 53, 67, 30, 73, 16,
+                69, 87, 40, 31, 04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23 };
+
+        List<Integer> list = Arrays.asList(nums);
+
+        // note to self - TreeSet and HashSet seems to force sorted order, whereas
+        // LinkedHashSet leaves the order as it was originally (though of course
+        // ALL remove duplicates because they are sets)
+
+        // Set<Integer> tree = new LinkedHashSet<>(list);
+        // System.out.println(tree);
+
+        // could do some form of row 1 has 1, 2 has 2, 3 has 3, etc... management
+        // a bit excessive to manage, but could be made to work
+
+        List<List<Integer>> rows = createStructureWithRows(list);
+
+        sum = navigateRowsGetSum(rows);
+
+        return sum;
+    }
+
+    private static List<List<Integer>> createStructureWithRows(List<Integer> numList) {
+        List<List<Integer>> listOfRows = new ArrayList<>();
+        int counter = 0;
+        int row = 0;
+
+        row++; // start with a row of 1
+        for (int i = 0; i < numList.size();) {
+            List<Integer> tmpRow = new ArrayList<>();
+            for (int j = 0; j < row; j++) {
+                tmpRow.add(numList.get(i++));
+                counter++;
+            }
+            listOfRows.add(tmpRow);
+            // if (i < list.size() - 1) {
+            if (counter < numList.size()) {
+                row++;
+            }
+        }
+        return listOfRows;
+    }
+
+    private static int navigateRowsGetSum(List<List<Integer>> rows) {
+        int sum = 0;
+        int position = 0;
+        int newPosition = 0;
+
+        // NOTE:
+        // clarification - problem is NOT to solve greatest
+        // values from top down; but rather highest overall
+        // sum of ANY path - I missed that first time;
+        // will take another look later...
+
+        // just need to add selectivity of highest value
+        // based on position in row above to complete
+
+        // navigate through rows in list of lists
+        for (int i = 0; i < rows.size(); i++) {
+            int maxVal = 0;
+            position = newPosition;
+            // for (int j = 0; j < rows.get(i).size(); j++) {
+            for (int j = position; j <= i; j++) {
+                if (rows.get(i).get(j) > maxVal && (j >= position && j <= position + 1)) {
+                    maxVal = rows.get(i).get(j);
+                    newPosition = j;
+                }
+            }
+            System.out.println(maxVal);
+            sum += maxVal;
+        }
+
+        return sum;
     }
 
     // private static int maxPathSum(Dictionary<Integer, ArrayList<Integer>> dict){
