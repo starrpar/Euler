d7bb95c4ff99b4fcf61b016de0569873f080c424
starrpar <starrparker@live.com>
2022-07-19 13:04:58 -0700

    still interim state

--- euler_18/src/main/java/euler_18/App.java
+++ euler_18/src/main/java/euler_18/App.java
@@ -45,6 +45,8 @@ public class App {
      * triangle containing one-hundred rows; it cannot be solved by brute force, and
      * requires a clever method! ;o)
      */
+
+    /* Temp Note - almost done with this, but will have to come back to it later */
     public static void main(String[] args) {
         int sum = maxPath();
         System.out.println(sum);
@@ -52,13 +54,17 @@ public class App {
 
     private static int maxPath() {
         int sum = 0;
-        // Integer[] nums = { 3, 7, 4, 2, 4, 6, 8, 5, 9, 3 };
+        Integer[] nums = { 3, 7, 4, 2, 4, 6, 8, 5, 9, 3 };
 
-        Integer[] nums = { 75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47, 65, 19, 01, 23, 75, 03, 34, 88, 02,
-                77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33,
-                47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39,
-                68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 04, 68, 89, 53, 67, 30, 73, 16,
-                69, 87, 40, 31, 04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23 };
+        // Integer[] nums = { 75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47,
+        // 65, 19, 01, 23, 75, 03, 34, 88, 02,
+        // 77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40,
+        // 80, 70, 33, 41, 48, 72, 33,
+        // 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11,
+        // 33, 28, 77, 73, 17, 78, 39,
+        // 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 04,
+        // 68, 89, 53, 67, 30, 73, 16,
+        // 69, 87, 40, 31, 04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23 };
 
         List<Integer> list = Arrays.asList(nums);
 
@@ -74,7 +80,7 @@ public class App {
 
         List<List<Integer>> rows = createStructureWithRows(list);
 
-        sum = navigateRowsGetSum(rows);
+        sum = findMaxPath(rows);
 
         return sum;
     }
@@ -127,6 +133,75 @@ public class App {
             }
             System.out.println(maxVal);
             sum += maxVal;
+        }
+
+        return sum;
+    }
+
+    private static int findMaxPath(List<List<Integer>> rows) {
+        int sum = 0;
+        int row = 0;
+        List<Integer> pivotRows = new ArrayList<>();
+        int pivotRow = rows.size();
+        Boolean wait = false;
+        List<Integer> pathSums = new ArrayList<>();
+
+        // NOTE:
+        // clarification - problem is NOT to solve greatest
+        // values from top down; but rather highest overall
+        // sum of ANY path through adjacent values in the next
+        // row down - I missed that first time;
+        // will take another look later...
+
+        // just need to add selectivity of highest value
+        // based on position in row above to complete
+
+        // navigate through rows in list of lists
+        for (int i = 0; i < rows.size();) {
+            for (int j = 0; j < rows.get(i).size();) {
+                for (int k = 0; k < pivotRows.size(); k++) {
+                    if (row == pivotRows.get(k)) {
+                        j++;
+                    }
+                }
+                sum += rows.get(i).get(j);
+                row++;
+                if (row < rows.size()) {
+                    i++;
+                } else {
+                    j = 0;
+                    break;
+                }
+            }
+            pathSums.add(sum);
+            System.out.println(sum);
+            // reset params
+            // if (!right) {
+            // right = true;
+            // } else {
+            // right = false;
+            // }
+            sum = 0;
+            i = 0;
+            if (wait) {
+                pivotRow--;
+                pivotRows.clear();
+                pivotRows.add(pivotRow);
+            }
+            if (row == rows.size() && wait) {
+                wait = false;
+                row = 0;
+                continue;
+            }
+            if (row == rows.size() && !wait) {
+                if (pivotRow != 3) {
+                    pivotRow = 3;
+                    pivotRows.add(pivotRow);
+                }
+                wait = true;
+            }
+
+            row = 0;
         }
 
         return sum;
