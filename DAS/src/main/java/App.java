f6923e2db221e1779577e678d8fda84dd1a9fb0c
Starr <starrparker@live.com>
2024-03-04 17:29:46 -0800

    add initial DAS files

---
+++ DAS/src/main/java/App.java
@@ -0,0 +1,43 @@
+
+// Requirements:
+
+// This shelter is in need of a user-friendly tool to accurately record and track the inflow and outflow of donations, and to generate insightful reports about their donation management.
+
+// Here are the core functionalities your solution should address:
+
+// Donation Registration: A feature that allows the shelter staff to record details of the donations, such as the donor's name, type of donation (money, food, clothing, etc.), quantity or amount donated, and the date of the donation.
+// Donation Distribution: A feature to log when and how much of the donations are distributed, capturing the type of donation, quantity or amount distributed, and the date of distribution.
+// Donation Reports: Your solution should have the capacity to generate two types of reports: (1) An inventory report displaying the current status of donations, grouped by type. (2) A donator report, summarizing the total contributions received from each donor.
+
+public class App {
+    public static void main(String[] args) {
+
+        register();
+        distribute();
+        generateReports();
+
+    }
+
+    // A feature that allows the shelter staff to record details of the donations,
+    // such as the donor's name, type of donation (money, food, clothing, etc.),
+    // quantity or amount donated, and the date of the donation.
+    private static void register() {
+
+    }
+
+    // A feature to log when and how much of the donations are distributed,
+    // capturing the type of donation, quantity or amount distributed, and the date
+    // of distribution.
+    private static void distribute() {
+
+    }
+
+    // Your solution should have the capacity to generate two types of reports:
+    // (1) An inventory report displaying the current status of donations, grouped
+    // by type.
+    // (2) A donator report, summarizing the total contributions received from each
+    // donor.
+    private static void generateReports() {
+
+    }
+}
--- euler_10/src/main/java/App.java
+++ euler_10/src/main/java/App.java
@@ -1,5 +1,4 @@
 
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -11,8 +10,8 @@ public class App {
      * Find the sum of all the primes below two million.
      */
     public static void main(String[] args) {
-        //long limit = 10L;
-        long limit = 2000000L;
+        // long limit = 10L;
+        long limit = 100000L;// 2000000L;
         List<Long> primes = new ArrayList<Long>();
 
         long startTime = System.currentTimeMillis();
@@ -23,7 +22,7 @@ public class App {
         long currentPrime, sum = 0;
         for (int i = 0; i < primes.size(); i++) {
             currentPrime = primes.get(i);
-  
+
             System.out.println(currentPrime);
             sum += currentPrime;
         }
@@ -36,8 +35,9 @@ public class App {
         Boolean divisible = false;
         for (long i = 2; i < limit; i++) {
             divisible = false;
-            for (int j = 2; j < i; j++) { // < vs. <= because dividing primes by themselves will eliminate them from selection
-                if((i%50000 == 0) && (j%(50000) == 0)){
+            for (int j = 2; j < i; j++) { // < vs. <= because dividing primes by themselves will eliminate them from
+                                          // selection
+                if ((i % 50000 == 0) && (j % (50000) == 0)) {
                     System.out.println("i,j: " + i + " :: " + j);
                 }
                 if (i % j == 0) {
