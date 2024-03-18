02db52dc488d23ea731c52ea916d6c431c653e26
Starr <starrparker@live.com>
2024-03-18 12:30:08 -0700

    updating notes for self

--- DAS/src/main/java/App.java
+++ DAS/src/main/java/App.java
@@ -12,16 +12,27 @@ import java.util.ArrayList;
 // This shelter is in need of a user-friendly tool to accurately record and track the inflow and outflow of donations, and to generate insightful reports about their donation management.
 
 // Here are the core functionalities your solution should address:
-// DONE - Donation Registration: A feature that allows the shelter staff to record details of the donations, such as the donor's name, type of donation (money, food, clothing, etc.), quantity or amount donated, and the date of the donation.
-// DONE - Donation Distribution: A feature to log when and how much of the donations are distributed, capturing the type of donation, quantity or amount distributed, and the date of distribution.
-// DONE - Donation Reports: Your solution should have the capacity to generate two types of reports: (1) An inventory report displaying the current status of donations, grouped by type. (2) A donator report, summarizing the total contributions received from each donor.
 
-// build jar file command: jar cvf DonationsDistribution.jar -C target\classes .
+// DONE - Donation Registration: A feature that allows the shelter staff to record details of the donations, such as the donor's name, 
+// type of donation (money, food, clothing, etc.), quantity or amount donated, and the date of the donation.
+// NOTES: Support input of Donor (name), type of donation (ENUM supporting Money, Clothing, Food and Misc, but could be expanded), value of donation (monetary equivalence),
+// date of donation, and "amount remaining" is calculated behind the scenes - primarily to adjust value available based on distributions of funds/resources.
+
+// DONE - Donation Distribution: A feature to log when and how much of the donations are distributed, capturing the type of donation, 
+// quantity or amount distributed, and the date of distribution.
+// NOTES: Added some trivial simulation/test scenario code, but functionality is present to evaluate if appropriate funds are present, against
+// which to draw for a given donation type and donor in the DB, so that could be used to render whatever more refined functionality was preferred
+// (simply build on the existing structure for data that is currently present)
+
+// DONE - Donation Reports: Your solution should have the capacity to generate two types of reports: (1) An inventory report 
+// displaying the current status of donations, grouped by type. (2) A donator report, summarizing the total contributions received from each donor.
+// NOTES: you can see in the "reports" (logged to log file currently - entitled "application-20240318.log" in the resources folder in Git - DAS/src/main/resources) 
+// that the data is present to be able to "present" the requested/expected data in a preferred format (nice UI, for example) relatively easily.
 
 // TO DO List:
 // DONE Move instructions to README file for GitHub repository
 // 
-// DONE - 0. Add JSON file data intake
+// DONE 0. Add JSON file data intake
 //   (FUTURE: could eventually be REST API from a UI for query support)
 // 
 // DONE 1. Add business logic to support not distributing funds of a given type unless such quantity currently exists
@@ -29,14 +40,16 @@ import java.util.ArrayList;
 //   DONE //ToDo: determine if amount expected of given DonationType is present in remaining/existing donations (not already distributed)
 //   DONE //ToDo: add logic to determine if distribution data was successfully inserted or not
 //   
-// 2. DONE - Improve reporting (even though trivial without better formatting - see below/FUTURE)
+// DONE 2. Improve reporting (even though trivial without better formatting - see below/FUTURE)
 //
-// 3. (NEXT)    Add unit testing
+// (NEXT) 3. Add unit testing
 //
-// 4. DONE - (Add more logging - minor)
+// DONE? 4. (Add more logging - minor, possibly not even necessary)
 // 
 // FUTURE: Augment reporting functionality with a UI to render the output if customer was wanting such.
 // 
+// NOTE for self:
+// build jar file command: jar cvf DonationsDistribution.jar -C target\classes .
 
 public class App {
 
